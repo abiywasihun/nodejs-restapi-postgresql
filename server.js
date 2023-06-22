bbcdc6269f3182b0394e4b26f255e76d7b80149c
Abiy Wasihun <abiywasihun0921@gmail.com>
2023-06-22 08:35:51 -0700

    add CORS policy

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use('/resources', express.static('resources'))
 
 
